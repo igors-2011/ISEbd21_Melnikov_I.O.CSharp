0d232bfbf9c773733177efefb560a90dd920fad7
Мк Игорь <igors-2011@mail.ru>
2019-04-20 12:05:26 +0400

    Lab_6

--- WindowsFormsTraktor/WindowsFormsTraktor/FormTraktorParking.Designer.cs
+++ WindowsFormsTraktor/WindowsFormsTraktor/FormTraktorParking.Designer.cs
@@ -36,17 +36,24 @@
             this.labelPlace = new System.Windows.Forms.Label();
             this.listBoxParkingLevels = new System.Windows.Forms.ListBox();
             this.buttonAddTransport = new System.Windows.Forms.Button();
+            this.menuStripWorkingWithParkingLevels = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
             this.groupBoxTraktorPickup.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakenTransport)).BeginInit();
+            this.menuStripWorkingWithParkingLevels.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBoxParking
             // 
             this.pictureBoxParking.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pictureBoxParking.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxParking.Location = new System.Drawing.Point(0, 24);
             this.pictureBoxParking.Name = "pictureBoxParking";
-            this.pictureBoxParking.Size = new System.Drawing.Size(712, 462);
+            this.pictureBoxParking.Size = new System.Drawing.Size(712, 438);
             this.pictureBoxParking.TabIndex = 0;
             this.pictureBoxParking.TabStop = false;
             // 
@@ -101,7 +108,7 @@
             // listBoxParkingLevels
             // 
             this.listBoxParkingLevels.FormattingEnabled = true;
-            this.listBoxParkingLevels.Location = new System.Drawing.Point(722, 18);
+            this.listBoxParkingLevels.Location = new System.Drawing.Point(719, 64);
             this.listBoxParkingLevels.Name = "listBoxParkingLevels";
             this.listBoxParkingLevels.Size = new System.Drawing.Size(150, 121);
             this.listBoxParkingLevels.TabIndex = 4;
@@ -109,13 +116,55 @@
             // 
             // buttonAddTransport
             // 
-            this.buttonAddTransport.Location = new System.Drawing.Point(722, 155);
+            this.buttonAddTransport.Location = new System.Drawing.Point(722, 191);
             this.buttonAddTransport.Name = "buttonAddTransport";
             this.buttonAddTransport.Size = new System.Drawing.Size(149, 31);
             this.buttonAddTransport.TabIndex = 5;
             this.buttonAddTransport.Text = "Добавить технику";
             this.buttonAddTransport.UseVisualStyleBackColor = true;
             this.buttonAddTransport.Click += new System.EventHandler(this.buttonAddTransport_Click);
+            // 
+            // menuStripWorkingWithParkingLevels
+            // 
+            this.menuStripWorkingWithParkingLevels.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStripWorkingWithParkingLevels.Location = new System.Drawing.Point(0, 0);
+            this.menuStripWorkingWithParkingLevels.Name = "menuStripWorkingWithParkingLevels";
+            this.menuStripWorkingWithParkingLevels.Size = new System.Drawing.Size(884, 24);
+            this.menuStripWorkingWithParkingLevels.TabIndex = 6;
+            this.menuStripWorkingWithParkingLevels.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "txt file | *.txt";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "txt file | *.txt";
             // 
             // FormTraktorParking
             // 
@@ -126,13 +175,18 @@
             this.Controls.Add(this.listBoxParkingLevels);
             this.Controls.Add(this.groupBoxTraktorPickup);
             this.Controls.Add(this.pictureBoxParking);
+            this.Controls.Add(this.menuStripWorkingWithParkingLevels);
+            this.MainMenuStrip = this.menuStripWorkingWithParkingLevels;
             this.Name = "FormTraktorParking";
             this.Text = "Стоянка";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
             this.groupBoxTraktorPickup.ResumeLayout(false);
             this.groupBoxTraktorPickup.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakenTransport)).EndInit();
+            this.menuStripWorkingWithParkingLevels.ResumeLayout(false);
+            this.menuStripWorkingWithParkingLevels.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,5 +200,11 @@
         private System.Windows.Forms.Label labelPlace;
         private System.Windows.Forms.ListBox listBoxParkingLevels;
         private System.Windows.Forms.Button buttonAddTransport;
+        private System.Windows.Forms.MenuStrip menuStripWorkingWithParkingLevels;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
--- WindowsFormsTraktor/WindowsFormsTraktor/MultiLevelTraktorParking.cs
+++ WindowsFormsTraktor/WindowsFormsTraktor/MultiLevelTraktorParking.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WindowsFormsTraktor
 {
@@ -12,12 +11,17 @@ namespace WindowsFormsTraktor
 
         private const int Places = 20;
 
+        private int PictureWidth;
+        private int PictureHeight;
+
         public MultiLevelTraktorParking(int LevelsNumber, int picwidth, int picheight)
         {
+            this.PictureWidth = picwidth;
+            this.PictureHeight = picheight;
             ParkingLevels = new List<TraktorParking<ITransport>>();
             for (int i = 0; i < LevelsNumber; i++)
             {
-                ParkingLevels.Add(new TraktorParking<ITransport>(Places, picwidth, picheight));
+                ParkingLevels.Add(new TraktorParking<ITransport>(Places, PictureWidth, PictureHeight));
             }
         }
 
@@ -31,6 +35,103 @@ namespace WindowsFormsTraktor
                 }
                 return null;
             }
+        }
+
+        public bool SaveData(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                WriteToFile("LevelsNumber:" + ParkingLevels.Count + Environment.NewLine, fs);
+                foreach (var level in ParkingLevels)
+                {
+                    WriteToFile("Level" + Environment.NewLine, fs);
+                    for (int i = 0; i < Places; i++)
+                    {
+                        var traktor = level[i];
+                        if (traktor != null)
+                        {
+                            if (traktor.GetType().Name == "Traktor")
+                            {
+                                WriteToFile(i + ":Traktor:", fs);
+                            }
+                            if (traktor.GetType().Name == "TraktorExcavator")
+                            {
+                                WriteToFile(i + ":TraktorExcavator:", fs);
+                            }
+                            WriteToFile(traktor + Environment.NewLine, fs);
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void WriteToFile(string text, FileStream stream)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(text);
+            stream.Write(info, 0, info.Length);
+        }
+
+        public bool LoadData(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return false;
+            }
+            string bufferTextFromFile = "";
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                byte[] b = new byte[fs.Length];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    bufferTextFromFile += temp.GetString(b);
+                }
+            }
+            bufferTextFromFile = bufferTextFromFile.Replace("\r", "");
+            var strs = bufferTextFromFile.Split('\n');
+            if (strs[0].Contains("LevelsNumber"))
+            {
+                int count = Convert.ToInt32(strs[0].Split(':')[1]);
+                if (ParkingLevels != null)
+                {
+                    ParkingLevels.Clear();
+                }
+                ParkingLevels = new List<TraktorParking<ITransport>>(count);
+            }
+            else
+            {
+                return false;
+            }
+            int counter = -1;
+            ITransport traktor = null;
+            for (int i = 1; i < strs.Length; ++i)
+            {
+                if (strs[i] == "Level")
+                {
+                    counter++;
+                    ParkingLevels.Add(new TraktorParking<ITransport>(Places, PictureWidth, PictureHeight));
+                    continue;
+                }
+                if (string.IsNullOrEmpty(strs[i]))
+                {
+                    continue;
+                }
+                if (strs[i].Split(':')[1] == "Traktor")
+                {
+                    traktor = new Traktor(strs[i].Split(':')[2]);
+                }
+                else if (strs[i].Split(':')[1] == "TraktorExcavator")
+                {
+                    traktor = new TraktorExcavator(strs[i].Split(':')[2]);
+                }
+                ParkingLevels[counter][Convert.ToInt32(strs[i].Split(':')[0])] = traktor;
+            }
+            return true;
         }
     }
 }
